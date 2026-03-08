07878d72de3049d0b06737f52d7148ccbb321aaa
cong ly <itshidou@gmail.com>
2026-03-08 18:23:32 +0700

    add layout admin and logo

---
+++ QLBanNuoc/QLBanNuoc/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLBanNuoc.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
